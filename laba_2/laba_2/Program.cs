7eafb40d85c2f71322f2c7a65f1666e60c535ed7
Alex <71846671+Alex-Kud@users.noreply.github.com>
2022-03-26 21:42:47 +0300

    The double type has been replaced with decimal
    Increased accuracy

--- laba_2/laba_2/Program.cs
+++ laba_2/laba_2/Program.cs
@@ -4,26 +4,26 @@ namespace laba_2
 {
     class Program
     {
-        private const double PI = 3.14159265358979323846;
-        private const double EPS = 1e-8;                    // Погрешность вычислений
+        private const decimal PI = 3.14159265358979323846m;
+        private const decimal EPS = 1e-17m;                    // Погрешность вычислений
         static void Main(string[] args)
         {
             try
             {
                 // a * (P(x))^2 * sin(b) - R(t) * e^z
                 Console.Write("Введите число a - double число: ");
-                double a = Convert.ToDouble(Console.ReadLine());
+                decimal a = Convert.ToDecimal(Console.ReadLine());
 
                 Console.WriteLine("Введите многочлен P:");
                 Console.Write("Введите степень n многочлена P - целое число: ");
                 int nP = Convert.ToInt32(Console.ReadLine());
                 nP++;                                        // Количество коэффициентов
-                Console.Write("Введите значение переменной x - double число: ");
-                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение переменной x - десятичная дробь: ");
+                decimal x = Convert.ToDecimal(Console.ReadLine());
                 int[] arrP = new int[nP];
                 for (int i = 0; i < nP; ++i)
                 {
-                    Console.Write($"Введите коэффициент перед x^{nP - i} - int число: ");
+                    Console.Write($"Введите коэффициент перед x^{nP - i - 1} - int число: ");
                     arrP[i] = Convert.ToInt32(Console.ReadLine());
                 }
 
@@ -31,8 +31,8 @@ namespace laba_2
                 int b = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Введите рациональную дробь R:");
-                Console.Write("Введите значение t в рациональной дроби - double число: ");
-                double t = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение t в рациональной дроби - десятичная дробь: ");
+                decimal t = Convert.ToDecimal(Console.ReadLine());
                 Console.WriteLine("Введите многочлен-числитель рациональной дроби R:");
                 Console.Write("Введите степень n многочлена-числителя R - целое число: ");
                 int nR1 = Convert.ToInt32(Console.ReadLine());
@@ -40,7 +40,7 @@ namespace laba_2
                 int[] arrR1 = new int[nR1];
                 for (int i = 0; i < nR1; ++i)
                 {
-                    Console.Write($"Введите коэффициент перед t^{nR1 - i} - int число: ");
+                    Console.Write($"Введите коэффициент перед t^{nR1 - i - 1} - int число: ");
                     arrR1[i] = Convert.ToInt32(Console.ReadLine());
                 }
                 Console.WriteLine("Введите многочлен-знаменатель рациональной дроби R:");
@@ -50,29 +50,29 @@ namespace laba_2
                 int[] arrR2 = new int[nR2];
                 for (int i = 0; i < nR2; ++i)
                 {
-                    Console.Write($"Введите коэффициент перед t^{nR2 - i} - int число: ");
+                    Console.Write($"Введите коэффициент перед t^{nR2 - i - 1} - int число: ");
                     arrR2[i] = Convert.ToInt32(Console.ReadLine());
                 }
 
-                Console.Write("Введите степень z показательной функции - double число: ");
-                double z = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите степень z показательной функции - десятичная дробь: ");
+                decimal z = Convert.ToDecimal(Console.ReadLine());
 
                 // a * (P(x))^2 * sin(b) - R(t) * e^z
                 // Результаты промежуточных вычислений
-                double P = Gorner(nP, x, arrP);
-                double P2 = P * P;
-                double sinB = Sin(b);
-                double R = RationalFraction(t, nR1, arrR1, nR2, arrR2);
-                double e = Exp(z);
-                double result = a * P2 * sinB - R * e;
+                decimal P = Gorner(nP, x, arrP);
+                decimal P2 = P * P;
+                decimal sinB = Sin(b);
+                decimal R = RationalFraction(t, nR1, arrR1, nR2, arrR2);
+                decimal e = Exp(z);
+                decimal result = a * P2 * sinB - R * e;
                 
                 // Вывод результатов промежуточных вычислений:
-                Console.WriteLine($"Значение многочлена P({x}): {P}");
-                Console.WriteLine($"Значение P({x})^2: {P2}");
-                Console.WriteLine($"Значение sin({b}): {sinB}");
-                Console.WriteLine($"Значение рациональной дроби R({t}): {R}");
-                Console.WriteLine($"Значение показательной функции e^{z}: {e}");
-                Console.WriteLine($"Значение выражения {a} * (P({x}))^2 * sin({b}) - R({t}) * e^{z}: {result}");
+                Console.WriteLine($"Значение многочлена P({x}): {Math.Round(P, 8)}");
+                Console.WriteLine($"Значение P({x})^2: {Math.Round(P2, 8)}");
+                Console.WriteLine($"Значение sin({b}): {Math.Round(sinB, 8)}");
+                Console.WriteLine($"Значение рациональной дроби R({t}): {Math.Round(R, 8)}");
+                Console.WriteLine($"Значение показательной функции e^{z}: {Math.Round(e, 8)}");
+                Console.WriteLine($"Значение выражения {a} * (P({x}))^2 * sin({b}) - R({t}) * e^{z}: {Math.Round(result, 8)}");
             }
             catch (FormatException)
             {
@@ -87,12 +87,12 @@ namespace laba_2
         /// <param name="x">Значение переменной</param>
         /// <param name="arr">Массив коэффициентов от высшей степени к низшей</param>
         /// <returns>Значение многочлена</returns>
-        private static double Gorner(int n, double x, int[] arr)
+        private static decimal Gorner(int n, decimal x, int[] arr)
         {
-            double s = arr[0];
+            decimal s = arr[0];
             for (int i = 1; i < n; ++i)
                 s = s * x + arr[i];
-            s = Math.Round(s, 8);       // Округление до 8 знаков
+            //s = Math.Round(s, 8);       // Округление до 8 знаков
             return s;
         }
 
@@ -105,7 +105,7 @@ namespace laba_2
         /// <param name="n2">Степень многочлена знаменателя</param>
         /// <param name="arr2">Массив коэффициентов знаменателя от высшей степени к низшей</param>
         /// <returns>Значение рациональной дроби</returns>
-        private static double RationalFraction(double x, int n1, int[] arr1, int n2, int[] arr2)
+        private static decimal RationalFraction(decimal x, int n1, int[] arr1, int n2, int[] arr2)
         {
             return Gorner(n1, x, arr1) / Gorner(n2, x, arr2);
         }
@@ -115,11 +115,11 @@ namespace laba_2
         /// </summary>
         /// <param name="x">Значение аргумента</param>
         /// <returns>Значение синуса</returns>
-        private static double Sin(double x)
+        private static decimal Sin(decimal x)
         {
             x *= PI / 180;              // Перевод из градусов в радианы
-            double s = 0;               // Начальная сумма
-            double q = x;               // Первый член ряда
+            decimal s = 0;               // Начальная сумма
+            decimal q = x;               // Первый член ряда
             int n = 1;                  // Показатель степени
             while (Math.Abs(q) > EPS)   // Условие выполнения цикла
             {
@@ -128,7 +128,7 @@ namespace laba_2
                 n++;                    // Наращивание  n:  1,2,3,4,...
             }
 
-            s = Math.Round(s, 8);       // Округление до 8 знаков
+            //s = Math.Round(s, 8);       // Округление до 8 знаков
             return s;                   // Возврат результата
         }
 
@@ -137,10 +137,10 @@ namespace laba_2
         /// </summary>
         /// <param name="x">Значение аргумента</param>
         /// <returns>Значение показательной функции</returns>
-        private static double Exp(double x)
+        private static decimal Exp(decimal x)
         {
-            double s = 0;               // Начальная сумма
-            double q = 1;               // Первый член ряда
+            decimal s = 0;               // Начальная сумма
+            decimal q = 1;               // Первый член ряда
             int n = 1;                  // Показатель степени
             while (Math.Abs(q) > EPS)   // Условие выполнения цикла
             {
@@ -149,7 +149,7 @@ namespace laba_2
                 n++;                    // Наращивание  n:  1,2,3,4,...
             }
 
-            s = Math.Round(s,8);        // Округление до 8 знаков
+            //s = Math.Round(s,8);        // Округление до 8 знаков
             return s;                   // Возврат результата
         }
     }
